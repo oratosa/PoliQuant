9260091ab13adb192ba3dc667f6e1576b78040c5
oratosa <c121034t@gmail.com>
2023-08-05 16:52:44 -0700

    changed the data type of id

--- sql/create_source_m_representatives_district.sql
+++ sql/create_source_m_representatives_district.sql
@@ -1,6 +1,9 @@
+drop table if exists poliquant.source.m_representatives_district
+;
+
 create table if not exists poliquant.source.m_representatives_district
 (
-  id string options(description="IDs doesn't follow the rule of JIS X 0401."),
+  id INT64 options(description="IDs doesn't follow the rule of JIS X 0401."),
   district string options(description="This column includes prefectures and areas."),
   type string options(description="There are 2 types like '小選挙区', '比例'.")
 )
