658de5861877f05e233a44a3ad9fd82c88feec35
oratosa <c121034t@gmail.com>
2023-08-15 16:48:55 -0700

    modified values in party column

--- sql/ctas_dwh_m_councilors.sql
+++ sql/ctas_dwh_m_councilors.sql
@@ -6,7 +6,11 @@ select
   m.profile_url,
   m.name,
   m.furigana,
-  m.party,
+  case 
+    when m.party = "民主" then "国民"
+    when m.party = "無所属" then "無"
+    else m.party
+    end as party,
   d.id as district_id,
   m.district,
   case when d.type = "小選挙区" then "1" --参議院選挙区は各都道府県ごとに1つのため、すべて1とする
