7253dcba4e9dc7cbfba1ec4c46981998ff338b6b
Taro Aso <49934427+oratosa@users.noreply.github.com>
2024-06-13 04:12:37 +0000

    added the install of apache-airflow[google].

--- install_airflow.sh
+++ install_airflow.sh
@@ -11,3 +11,4 @@ sleep 3
 
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow==${AIRFLOW_VERSION}" apache-airflow[google]
