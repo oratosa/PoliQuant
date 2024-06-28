e1aa573c88aebab65a3143b4337038c7704739a0
Taro Aso <49934427+oratosa@users.noreply.github.com>
2024-06-28 22:48:18 +0000

    renew it

--- airflow/dags/el_meeting_list.py
+++ airflow/dags/el_meeting_list.py
@@ -1,9 +1,12 @@
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
 from datetime import datetime
 
 from src.gcs_connection import GCSConnection
 from src.ndl_api import NdlApi
+from src.read_sql_file import read_sql_file
 
 default_args = {
     "start_date": datetime(2024, 1, 1),
@@ -11,29 +14,16 @@ default_args = {
 }
 
 with DAG(
-    "extract_n_load_meeting_list_to_BigQuery",
+    "make_dwh_meeting_list_in_BigQuery",
     default_args=default_args,
     schedule_interval="0 0 3 * *",
     catchup=False,
 ) as dag:
 
-    def load_meeting_files_to_source_table():
-        print("Loaded the files of the meeting list into a source table in BigQuery.")
-
-    def transform_the_source_table_of_meeting_list():
-        print(
-            "Transformed the rows in the source table of meeting list and inserted them into the table in the dwh layer."
-        )
-
-    def transform_the_dwh_table_of_meeting_list():
-        print(
-            "Transformed the rows in the dwh table of meeting_list and inserted them into the table in the mart layer."
-        )
-
     # Instantiate the class to pass a method to PythonOperator.
     ndl_api = NdlApi()
     t_extract_meeting_list = PythonOperator(
-        task_id="extract_meeting_list_task",
+        task_id="t_extract_meeting_list",
         python_callable=ndl_api.get_meeting_list,
         op_kwargs={
             "start_date": "2022-01-01",
@@ -45,7 +35,7 @@ with DAG(
     # Instantiate the class to pass a method to PythonOperator.
     gcs_conn = GCSConnection()
     t_put_meeting_list = PythonOperator(
-        task_id="put_meeting_list_task",
+        task_id="t_put_meeting_list",
         python_callable=gcs_conn.upload_local_files_to_blob,
         op_kwargs={
             "local_folder_path": "/workspaces/PoliQuant/data/meeting_list",
@@ -54,25 +44,55 @@ with DAG(
         },
     )
 
-    t_load_meeting_lists = PythonOperator(
-        task_id="load_meeting_list_task",
-        python_callable=load_meeting_files_to_source_table,
+    sql_load_source_meeting_list = read_sql_file(
+        "/workspaces/PoliQuant/sql/load_source_meeting_list.sql"
+    )
+    t_load_source_meeting_lists = BigQueryInsertJobOperator(
+        task_id="t_load_source_meeting_list",
+        configuration={
+            "query": {
+                "query": sql_load_source_meeting_list,
+                "useLegacySql": False,
+            }
+        },
+        gcp_conn_id="google_cloud_default",
+        location="US",
     )
 
-    t_transform_source_meeting = PythonOperator(
-        task_id="transform_source_meeting_list_task",
-        python_callable=transform_the_source_table_of_meeting_list,
+    sql_insert_dwh_meeting_list = read_sql_file(
+        "/workspaces/PoliQuant/sql/insert_dwh_meeting_list.sql"
+    )
+    t_insert_dwh_meeting_list = BigQueryInsertJobOperator(
+        task_id="t_insert_dwh_meeting_list",
+        configuration={
+            "query": {
+                "query": sql_insert_dwh_meeting_list,
+                "useLegacySql": False,
+            }
+        },
+        gcp_conn_id="google_cloud_default",
+        location="US",
     )
 
-    t_transform_dwh_meeting = PythonOperator(
-        task_id="transform_dwh_meeting_list_task",
-        python_callable=transform_the_dwh_table_of_meeting_list,
-    )
+    # The following task will be separated to another dag file to make the tables in the mart layer.
+    # sql_ctas_mart_meeting_list = read_sql_file(
+    #     "/workspaces/PoliQuant/sql/ctas_mart_meeting_list.sql"
+    # )
+    # t_ctas_mart_meeting_list = BigQueryInsertJobOperator(
+    #     task_id="t_ctas_mart_meeting_list",
+    #     configuration={
+    #         "query": {
+    #             "query": sql_ctas_mart_meeting_list,
+    #             "useLegacySql": False,
+    #         }
+    #     },
+    #     gcp_conn_id="google_cloud_default",
+    #     location="US",
+    # )
 
     (
         t_extract_meeting_list
         >> t_put_meeting_list
-        >> t_load_meeting_lists
-        >> t_transform_source_meeting
-        >> t_transform_dwh_meeting
+        >> t_load_source_meeting_lists
+        >> t_insert_dwh_meeting_list
     )
