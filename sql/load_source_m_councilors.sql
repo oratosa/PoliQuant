d3ded61e229f50c42eed5c165c4b16ae3322ec7b
Taro Aso <49934427+oratosa@users.noreply.github.com>
2024-07-11 17:40:52 +0000

    modified the file name.

--- sql/load_source_m_councilors.sql
+++ sql/load_source_m_councilors.sql
@@ -13,6 +13,6 @@ LOAD DATA INTO `poliquant.source.m_councilors`
 )
 FROM FILES (
   format = 'CSV',
-  uris = ['gs://poliquant/data/input/politician_list/councilor/councilors_master_*.csv']
+  uris = ['gs://poliquant/data/input/politician_list/councilors/councilors_*.csv']
   )
 ;
