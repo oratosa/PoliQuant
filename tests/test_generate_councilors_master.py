f1ec412624d8ea94a2dcccab0c7e49ea4ed2e714
Taro Aso <49934427+oratosa@users.noreply.github.com>
2024-07-11 17:43:06 +0000

    modified the class name to be tested.

--- tests/test_generate_councilors_master.py
+++ tests/test_generate_councilors_master.py
@@ -1,26 +1,26 @@
-from src.generate_councilors_master import CouncilorsNameList
+from parse_councilors import ParserCouncilors
 
 import datetime
 
 
-class TestCouncilorsNameList:
+class TestParserCouncilors:
     def test_init(self):
-        councilors = CouncilorsNameList(211)
+        councilors = ParserCouncilors(211)
         assert councilors.session == 211
         assert (
             councilors.source_url
             == "https://www.sangiin.go.jp/japanese/joho1/kousei/giin/211/giin.htm"
         )
         assert councilors.members == []
-        assert councilors.update_date == None
+        assert councilors.update_date is None
 
     def test_add_update_date(self):
-        councilors = CouncilorsNameList(211)
+        councilors = ParserCouncilors(211)
         councilors.add_update_date()
         assert councilors.update_date == str(datetime.date.today())
 
     def test_add_members(self):
-        councilors = CouncilorsNameList(211)
+        councilors = ParserCouncilors(211)
         councilors.add_members()
         assert not councilors.members[0] == [
             "https://www.sangiin.go.jp/japanese/joho1/kousei/giin/profile/7016001.htm",
